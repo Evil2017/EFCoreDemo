d56ed7266de51a378aef184e6303517aac3d0910
lsr <2766673111@qq.com>
2020-08-23 23:26:46 +0800

    unitofwork

---
+++ WebMvc/DAL/IStudentRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMvc.Models;
+
+namespace WebMvc.DAL
+{
+    public interface IStudentRepository : IDisposable
+    {
+        IEnumerable<Student> GetStudents();
+        Student GetStudentByID(int studentId);
+        void InsertStudent(Student student);
+        void DeleteStudent(int studentID);
+        void UpdateStudent(Student student);
+        void Save();
+    }
+}
